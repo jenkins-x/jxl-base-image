1518f033cf1f9ce11793f54153a07432f2f8a3c9
James Strachan <james.strachan@gmail.com>
2020-06-22 13:49:04 +0100

    fix: add promote destination

--- promote.sh
+++ promote.sh
@@ -2,4 +2,6 @@
 
 echo "promoting the new version ${VERSION} to downstream repositories"
 
+jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio-labs-private/jxl-base:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jx-promote.git
+
 jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio-labs-private/jxl-base:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jxl-base-image-jx.git
